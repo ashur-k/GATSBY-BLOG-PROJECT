e5e83dfa4bd02462bc8a46ed75c5078d26296153
ashur-k <ashurkanwal@yahoo.com>
2021-07-26 12:53:37 +0100

    final push before deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
-import * as React from "react"
-import { graphql } from "gatsby"
+import * as React from "react";
+import { graphql, Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import styled from "styled-components"
+
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color:blue;
+`
+
 
 export default ({ data }) => {
-  console.log('DATA:', data)
   return (
   <Layout>
     <Seo title="Home" />
@@ -15,7 +26,9 @@ export default ({ data }) => {
       {
         data.allMarkdownRemark.edges.map(({node}) => (
           <div key={node.id}>
-            <span>{ node.frontmatter.title } - { node.frontmatter.date }</span>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>{ node.frontmatter.title } - { node.frontmatter.date }</BlogTitle>
+          </BlogLink>
             <p>{node.excerpt}</p>
           </div>
         ))
@@ -26,15 +39,18 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
           id
           frontmatter {
-            description
-            date
+            description            
             title
+            date
+          }
+          fields{
+            slug
           }
         excerpt
       }
